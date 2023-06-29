56f7741a38cf88bb6c45c409e61f72b48c711779
lesleysin <nikandsin@gmail.com>
2023-06-29 16:40:20 +0200

    add db connection

--- cmd/main.go
+++ cmd/main.go
@@ -2,41 +2,60 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
+	"github.com/DogGoOrg/doggo-account/internal/db"
 	"github.com/DogGoOrg/doggo-account/proto/proto_services/Account"
+	"github.com/DogGoOrg/doggo-orm/models"
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
-)
-
-var (
-	port = flag.Int("port", 50051, "The server port")
+	"gorm.io/gorm"
 )
 
 type server struct {
 	Account.UnimplementedAccountServer
+	Database *gorm.DB
 }
 
 // ping method implementation
 func (receiver *server) Ping(ctx context.Context, in *Account.PingRequest) (*Account.PingResponse, error) {
-	fmt.Println("OK")
 	return &Account.PingResponse{
 		Status: "OK",
 	}, nil
 }
 
 func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	appMode := os.Getenv("MODE")
+
+	if appMode != "prod" {
+		if err := godotenv.Load("dev.env"); err != nil {
+			panic(err.Error())
+		}
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", os.Getenv("PORT")))
+
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	serv := new(server)
+
+	db := db.InitDB()
+
+	serv.Database = db
+
+	db.AutoMigrate(&models.Account{}, &models.Pet{})
+
 	s := grpc.NewServer()
-	Account.RegisterAccountServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+
+	Account.RegisterAccountServer(s, serv)
+	log.Printf("server listening at %v", listener.Addr())
+
+	if err := s.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,27 @@ module github.com/DogGoOrg/doggo-account
 go 1.20
 
 require (
+	github.com/DogGoOrg/doggo-orm v0.1.1
+	github.com/joho/godotenv v1.5.1
 	google.golang.org/grpc v1.56.1
-	google.golang.org/protobuf v1.30.0
+	google.golang.org/protobuf v1.31.0
+	gorm.io/gorm v1.25.1
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.1 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230626202813-9b080da550b3 // indirect
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	gorm.io/driver/postgres v1.5.2
 )
